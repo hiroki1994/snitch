f620b600539fd0efa195b3df55a59039e9d71623
sweatydistortion@gmail.com <sweatydistortion@gmail.com>
2021-01-14 13:11:46 +0900

    Amend SQLfiles and Add images

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,33 +1,39 @@
-CREATE TABLE IF NOT EXISTS omiyage (
-  omiyaID INT PRIMARY KEY,
-  guest VARCHAR(50),
-  name VARCHAR(100),
-  price VARCHAR(500),
-  image VARCHAR(50),
+CREATE TABLE IF NOT EXISTS userData (
+	userId INT PRIMARY KEY AUTO_INCREMENT,
+	userName VARCHAR(20) UNIQUE,
+	mailAddress VARCHAR(100),
+	password VARCHAR(100),
+	role VARCHAR(20),
+	unavailableFlag INT
+);
+
+CREATE TABLE IF NOT EXISTS guest (
+	guestId INT PRIMARY KEY AUTO_INCREMENT,
+	guestName VARCHAR(20),
+	unavailableFlag INT
+);
+
+CREATE TABLE IF NOT EXISTS gift (
+  giftId INT PRIMARY KEY AUTO_INCREMENT,
+  guestId INT,
+  giftName VARCHAR(50),
+  price VARCHAR(100),
+  image VARCHAR(20),
   description VARCHAR(3000),
   shop VARCHAR(100),
   address VARCHAR(500),
   phone VARCHAR(50),
-  keyword VARCHAR(1000)
+  keyword VARCHAR(1000),
+  unavailableFlag INT,
+  CONSTRAINT fk_gift_guest FOREIGN KEY fk_guestId(guestId) REFERENCES guest (guestId)
 );
 
-CREATE TABLE IF NOT EXISTS userData (
-	userId VARCHAR(50) PRIMARY KEY,
-	mailAddress VARCHAR(100),
-	password VARCHAR(100),
-	role VARCHAR(50)
-);
 
-CREATE TABLE IF NOT EXISTS FavOmiyage (
-  favId VARCHAR(50) PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS favGift (
+  favId INT PRIMARY KEY AUTO_INCREMENT,
   userId VARCHAR(50),
-  omiyaID INT,
-  guest VARCHAR(50),
-  name VARCHAR(100),
-  price VARCHAR(500),
-  image VARCHAR(50),
-  description VARCHAR(3000),
-  shop VARCHAR(100),
-  address VARCHAR(500),
-  phone VARCHAR(50)
+  giftId INT,
+  unavailableFlag INT,
+  CONSTRAINT fk_fav_user FOREIGN KEY fk_userId(userId) REFERENCES guest (guestId)
+  CONSTRAINT fk_fav_gift FOREIGN KEY fk_giftId(giftId) REFERENCES gift (giftId)
 );
