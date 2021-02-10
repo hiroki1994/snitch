33978933aa449bb92bf59f865b1865bd24f2151b
hiroki1994 <sweatydistortion@gmail.com>
2021-02-10 17:21:48 +0900

    FavGiftServiceTest

---
+++ src/test/java/com/example/demo/FavGiftServiceTest.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.example.demo.login.domain.service.FavGiftService;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class FavGiftServiceTest {
+
+	@Autowired
+	FavGiftService favGiftService;
+
+
+	@Test
+	public void お気に入り登録成功() throws Exception {
+		String userName = "userName";
+		int giftId = 100;
+		boolean expected = true;
+		boolean actual = favGiftService.create(userName, giftId);
+		assertEquals(expected, actual);
+
+	}
+
+	@Test
+	public void お気に入り登録失敗() throws Exception {
+		String userName = "userName";
+		int giftId = -100;
+		boolean expected = false;
+		boolean actual = favGiftService.create(userName, giftId);
+		assertEquals(expected, actual);
+
+	}
+}
