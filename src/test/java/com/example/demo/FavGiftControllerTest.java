1f39bf012c42e048859a3bf9e644b6d508ca8f7f
hiroki1994 <sweatydistortion@gmail.com>
2021-02-15 18:48:12 +0900

    Add FavGiftControllerTest

---
+++ src/test/java/com/example/demo/FavGiftControllerTest.java
@@ -0,0 +1,69 @@
+package com.example.demo;
+
+
+import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.login.controller.FavGiftController;
+import com.example.demo.login.domain.service.FavGiftService;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
+public class FavGiftControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Mock
+	FavGiftService favGiftService;
+
+	@InjectMocks
+	FavGiftController favGiftController;
+
+	@BeforeEach
+	public void initMock() {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	@WithMockUser(username="userName3")
+	@Sql({"/Delete.sql", "/Schema.sql", "/Insert.sql"})
+	public void お気に入り追加() throws Exception {
+
+
+		String userName = "userName3";
+
+		int giftId = 1002;
+
+		boolean result = true;
+
+		when(favGiftService.create(userName, giftId)).thenReturn(result);
+
+		mockMvc.perform(post("/favGift")
+			.param("userName", userName)
+			.param("giftId", "1002")
+			.with(csrf()))
+			.andExpect(status().isFound())
+			.andExpect(redirectedUrl("/giftDetail/" + giftId));
+	}
+}
